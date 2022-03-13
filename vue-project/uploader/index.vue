24dd6129947397c9bee17e1ef62de14f273ce38b
mewcoder <1010953107@qq.com>
2022-03-13 13:33:50 +0800

    feat: 前端分片上传

--- vue-project/uploader/index.vue
+++ vue-project/uploader/index.vue
@@ -9,27 +9,60 @@
 <script setup>
 import { ElMessage } from "element-plus";
 
-// import { ref } from "vue";
+import { ref, computed } from "vue";
 
+const chunkSize = 1024 * 200;
+
+const index = ref(0);
+
+const start = computed(() => {
+  return chunkSize * index.value;
+});
 defineProps({
   msg: String,
 });
 
 const handleUpload = () => {
   const file = document.getElementById("up").files[0];
+
+  if (!file) return;
+
+  let startByte = start.value;
+
+  console.log("startByte:", startByte);
+
+  if (startByte > file.size) {
+    ElMessage.success("上传完成");
+    index.value = 0;
+    return;
+  }
+
   const formData = new FormData();
-  formData.append("file", file);
-  console.log(file);
+  const chunkedFile = getChunkedFile(file, startByte);
+  formData.append("file", chunkedFile);
+
   fetch("/api/upload", {
     method: "POST",
     body: formData,
   })
-    .then((res) => {
-      ElMessage.success("上传成功");
+    .then(() => {
+      // ElMessage.info(`上传成功: ${blobName}`);
+      index.value++;
+      setTimeout(handleUpload, 500);
+      // handleUpload();
     })
     .catch((err) => {
       ElMessage.error(err);
     });
+};
+
+const getChunkedFile = (file, startByte) => {
+  const [fileName, fileType] = file.name.split(".");
+  const blob = file.slice(startByte, startByte + chunkSize);
+  const blobName = `${fileName}_${index.value}.${fileType}`;
+  const blobFile = new File([blob], blobName);
+  console.log("上传中...", blobName);
+  return blobFile;
 };
 </script>
 <style scoped></style>
